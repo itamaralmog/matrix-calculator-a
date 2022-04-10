09d2cdcb33bd8ea263e6df83ccf2f57e38a1ddd4
itamaralmog <74138270+itamaralmog@users.noreply.github.com>
2022-04-10 22:19:46 +0300

    try1 to fix output and input

--- Test.cpp
+++ Test.cpp
@@ -704,26 +704,26 @@ TEST_CASE("function <<"){
 
 }
 TEST_CASE("function >>"){
-    istringstream is{"[1 1 1 1], [1 1 1 1], [1 1 1 1]"};
+    istringstream is{"[1 1 1 1], [1 1 1 1], [1 1 1 1]\n"};
     is >> mat1;
     CHECK(mat1==mat_cin);
-    istringstream is1{"[ 1 1 1 1 ],[ 1 1 1 1 ], [ 1 1 1 1 ]"};
+    istringstream is1{"[1 1 1 1],[1 1 1 1], [1 1 1 1]\n"};
     CHECK_THROWS(is1 >> mat1);
-    istringstream is2{"[ 1 1 1 1 ], [ 1 1 1 1 ] [ 1 1 1 1 ],"};
+    istringstream is2{"[1 1 1 1], [1 1 1 1] [1 1 1 1],\n"};
     CHECK_THROWS(is2 >> mat1);
-    istringstream is3{"[ 1 11 1 ]  [ 1 1 1 1 ], [ 1 1 1 1 ]"};
+    istringstream is3{"[1 11 1]  [1 1 1 1], [1 1 1 1]\n"};
     CHECK_THROWS(is3 >> mat1);
-    istringstream is4{"[ 1 1 1 1 ], [ 1 1 1 1 ], [1 1 1 1 ]"};
+    istringstream is4{"[1 1 1 1], [1 1 1 1], [1 1 1 1]\n"};
     CHECK_THROWS(is4 >> mat1);
-    istringstream is5{",,[ 1 1 1 1 ] [ 1 1 1 1 ] [ 1 1 1 1 ]"};
+    istringstream is5{",,[1 1 1 1] [1 1 1 1] [1 1 1 1]\n"};
     CHECK_THROWS(is5 >> mat1);
-    istringstream is6{"[[ 1 1 1 1 ],  1 1 1 1 ], [ 1 1 1 1 ]"};
+    istringstream is6{"[[1 1 1 1],  1 1 1 1], [1 1 1 1]\n"};
     CHECK_THROWS(is6 >> mat1);
-    istringstream is7{"[ 1 1 1 1 ], [ 1 1 1 1 , [ 1 1 1 1 ]]"};
+    istringstream is7{"[ 1 1 1 1 ], [ 1 1 1 1 , [ 1 1 1 1 ]]\n"};
     CHECK_THROWS(is7 >> mat1);
-    istringstream is8{"     [ 1 1 1 1 ], [ 1 1 1 1 ], [ 1 1 1 1 ]"};
+    istringstream is8{"[1 1 1 1], [1 1 1 1], [1 1 1 1]\n"};
     CHECK_NOTHROW(is8 >> mat1);
-    istringstream is9{"     [1 1 1        1], [1 1 1 1], [1 1 1 1]"};
+    istringstream is9{"[1 1 1 1], [1 1 1 1], [1 1 1 1]\n"};
     CHECK_NOTHROW(is9 >> mat1);
 }
 
